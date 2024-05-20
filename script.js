c33ec3beba49d37e0f4191a3f313c73e13a4afb8
MaksyDarko <darko.punktooy@gmail.com>
2024-05-20 21:49:13 +0200

    #added file

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-let message = "Hello World";
-console.log(message);
+let outsideTemperature = 26;
 
-const pi = 5.14;
-
-let bigNumber = 12345678901234567890n;
-
-let truth = true;
-let falsy = false;
-
-let doLater;
-
-const colours = ["red", "green", "blue"];
-
-const numbers = [1, 2, 3];
-
-const user = {
-  name: "John",
-  age: 30,
-};
-
-console.log(user.age);
-
-{
-  let a = 5;
-  {
-    let b = 2;
-    let product;
-    {
-      let c = 3;
-      product = a * b * c;
-    }
-    console.log(product);
-  }
+if (outsideTemperature <= 0) {
+  console.log("It's freezing outside!");
+} else if (outsideTemperature > 0 && outsideTemperature <= 15) {
+  console.log("It's cold outside!");
+} else if (outsideTemperature > 16 && outsideTemperature <= 25) {
+  console.log("It's warm outside!");
+} else {
+  console.log("It's hot outside!");
 }
+
+let ageStatus = 60;
+
+if (ageStatus >= 0 && ageStatus < 2) {
+  console.log("Infant");
+} else if (ageStatus >= 3 && ageStatus <= 12) {
+  console.log("Child");
+} else if (ageStatus >= 13 && ageStatus <= 19) {
+  console.log("Teenager");
+} else if (ageStatus >= 20 && ageStatus <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+let number = 122;
+let result = number % 2 === 0 ? "Number is even" : "Number is odd";
+console.log(result);
