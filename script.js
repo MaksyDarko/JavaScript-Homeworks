b7be304e67990dfb059145630e50e1e66321aa9c
MaksyDarko <darko.punktooy@gmail.com>
2024-07-07 10:21:07 +0200

    #added file

--- script.js
+++ script.js
@@ -1,124 +1,46 @@
-// Task 1
+const firstDiv = document.getElementById(`firstDiv`);
 
-const movies = [
-  {
-    title: "Inception",
-    director: "Christopher Nolan",
-    year: 2010,
-    rating: 8.8,
-    duration: 148,
-  },
-  {
-    title: "Interstellar",
-    director: "Christopher Nolan",
-    year: 2014,
-    rating: 8.6,
-    duration: 169,
-  },
-  {
-    title: "The Dark Knight",
-    director: "Christopher Nolan",
-    year: 2008,
-    rating: 9.0,
-    duration: 152,
-  },
-  {
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: 1994,
-    rating: 8.9,
-    duration: 154,
-  },
-  {
-    title: "The Shawshank Redemption",
-    director: "Frank Darabont",
-    year: 1994,
-    rating: 9.3,
-    duration: 142,
-  },
-  {
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: 1972,
-    rating: 9.2,
-    duration: 175,
-  },
-  {
-    title: "The Matrix",
-    director: "Lana Wachowski, Lilly Wachowski",
-    year: 1999,
-    rating: 8.7,
-    duration: 136,
-  },
-  {
-    title: "Fight Club",
-    director: "David Fincher",
-    year: 1999,
-    rating: 8.8,
-    duration: 139,
-  },
-  {
-    title: "Forrest Gump",
-    director: "Robert Zemeckis",
-    year: 1994,
-    rating: 8.8,
-    duration: 142,
-  },
-  {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    director: "Peter Jackson",
-    year: 2001,
-    rating: 8.8,
-    duration: 178,
-  },
-];
+const onHover = function () {
+  firstDiv.style.backgroundColor = `purple`;
+};
 
-// Task 1 - Find Method
-// We type in the movie director that we wan't to find.
+const offHover = function () {
+  firstDiv.style.backgroundColor = ``;
+};
 
-let findByDirector = movies.find(
-  (movies) => movies.director === `Robert Zemeckis`
-);
-console.log(findByDirector);
+const cards = document.querySelectorAll(`.card`);
 
-// Task 2 - Some Method
-// Here we check if there are any movies released before the year 2000.
-
-const hasOldMovies = movies.some((movie) => movie.year < 2000);
-console.log(hasOldMovies);
-
-// Task 3 - Every Method
-// Here we check if all movies have a rating greater than 8.5.
-
-const hihglyRated = movies.every((movies) => movies.rating > 8.5);
-console.log(hihglyRated);
-
-// Task 4 - ForEach Method
-// Here we give the movie name and the duration on the movie.
-
-const durationMovie = movies.forEach((movie) => {
-  console.log(
-    `The duration of the ${movie.title} is : ${movie.duration} minutes.`
-  );
+cards.forEach((card) => {
+  const randomText = Math.random().toString(36).substring(5);
+  card.textContent = randomText;
+  card.style.color = `red`;
 });
 
-// Task 5 - Sort  Method
-// Here we sort the movies by their release year.
-// The sort method gives us the movies list starting from the lowest year realeased to the highest.
+const firstCard = document.querySelector(`.card`);
 
-const sortedByYear = movies.sort((a, b) => a.year - b.year);
-console.log(sortedByYear);
+if (firstCard) {
+  const spanElement = document.createElement(`span`);
+  spanElement.textContent = `Hello from span!`;
+  spanElement.style.backgroundColor = `green`;
+  firstCard.appendChild(spanElement);
+}
 
-// Task 6 - Map Method
-// With the map method we create a new array that shows only the titles of the movies.
+const button = document.createElement(`button`);
+button.textContent = `Click Me`;
 
-const movieTitles = movies.map((movie) => movie.title);
-console.log(movieTitles);
+firstDiv.parentNode.insertBefore(button, firstDiv);
+button.classList.add(`style-button`);
 
-// Task 7 - Filter Method
-// With the filter method we filtered every movie that has the director Ch Nolan.
+const toggleBtn = document.getElementById("toggleBtn");
+const toggleParagraph = document.getElementById("toggleParagraph");
 
-const chNolanMovies = movies.filter(
-  (movie) => movie.director === `Christopher Nolan`
-);
-console.log(chNolanMovies);
+toggleBtn.textContent = `Click`;
+toggleParagraph.textContent = `hello`;
+
+toggleBtn.addEventListener("click", function () {
+  if (toggleParagraph.style.display === "none") {
+    toggleParagraph.style.display = "block";
+  } else {
+    toggleParagraph.style.display = "none";
+  }
+});
